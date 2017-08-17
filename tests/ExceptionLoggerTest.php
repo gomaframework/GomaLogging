31cc7319c667f56bad7b58f8aafe85d452681fc4
Daniel Gruber <daniel@ibpg.eu>
2017-08-17 02:20:28 +0200

    - Add ExceptionLoggerTests

---
+++ tests/ExceptionLoggerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Goma\Logging\Test;
+
+use Goma\ENV\GomaENV;
+use Goma\Logging\ExceptionLogger;
+
+defined("IN_GOMA") or die();
+
+/**
+ * Tests ExceptionLogger-Class.
+ *
+ * @package	goma/logging
+ * @link 	http://goma-cms.org
+ * @license LGPL http://www.gnu.org/copyleft/lesser.html see 'license.txt'
+ * @author 	Goma-Team
+ */
+class ExceptionLoggerTest extends \GomaUnitTest
+{
+    /**
+     * Tests if logging of an ignorable exception is not writing a file.
+     */
+    public function testIgnorable() {
+        $expectedFile = GomaENV::getDataDirectory() . LOG_FOLDER . "/debug/" . date("m-d-y") . "/".date("H_i_s")."_1.log";
+        if(is_file($expectedFile)) {
+            unlink($expectedFile);
+        }
+
+        $exception = new \Exception();
+        $exception->isIgnorable = true;
+
+        ExceptionLogger::logException($exception);
+        $this->assertFalse(is_file($expectedFile));
+    }
+
+    /**
+     * Tests logging of an exception is writing a file with exception message.
+     */
+    public function testLogsException() {
+        $expectedFile = GomaENV::getDataDirectory() . LOG_FOLDER . "/debug/" . date("m-d-y") . "/".date("H_i_s")."_1.log";
+        if(is_file($expectedFile)) {
+            unlink($expectedFile);
+        }
+
+        $exception = new \Exception("My Exception");
+
+        ExceptionLogger::logException($exception);
+        $this->assertTrue(is_file($expectedFile));
+        $this->assertRegExp("/My Exception/", file_get_contents($expectedFile));
+    }
+}
